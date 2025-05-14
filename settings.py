9d54048b34e4eee7df87995d2010450ff6f5d6c5
Phobeus1 <deltastrain@gmail.com>
2025-05-14 17:12:57 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 # Waittime is time in seconds between runs
-# Duration is time in minutes that the test continues to run
+# Duration is time in minutes that the test continues to run, default of 1 month
 # If duration is commented out, test runs indefinitely
 
 wait_time = 60
-duration = 60
+duration = 43800
 
 # IOS device and credentials
 # ssh_targets is a set of IP Addresses or DNS resolve-able hostnames
@@ -20,14 +20,18 @@ command = 'show running'
 # If urls is an empty set [] or commented out this test is skipped
 
 urls=[
-	'https://twitter.com',
+	'https://x.com/',
+	'https://portal.azure.com/#home',
+	'https://zoom.us/test',
+	'https://test.salesforce.com/',
+	'https://venmo.com/',
+	'https://teams.microsoft.com/v2/',
 	'https://www.linkedin.com',
 	'https://www.office.com',
 	'https://www.microsoft.com/en-us/microsoft-365',
 	'https://www.cisco.com',
 	'https://www.google.com',
 	'https://docs.google.com/spreadsheets/d/1rG2vCzmlmfsAEp2RU6XTVTLXx8J0ekPbCIpgzbdeJFQ/edit?usp=sharing',
-	'https://play.google.com',
 	'https://photos.google.com',
 	'https://www.dropbox.com/s/zlg3hw01yyvorcg/Adirondack%20Chair%20Plans%20-%20Popular%20Mechanics.pdf?dl=0',
 	'https://console.aws.amazon.com',
